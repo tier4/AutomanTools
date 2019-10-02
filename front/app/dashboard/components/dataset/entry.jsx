fb82e904e7c249bcdf2eec803f53cbeeee38cd48
tsshimo <tshimo@asg.cs.tuat.ac.jp>
2019-10-02 05:23:14 +0000

    remove unused Job button

--- front/app/dashboard/components/dataset/entry.jsx
+++ front/app/dashboard/components/dataset/entry.jsx
@@ -1,49 +1,27 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Add from '@material-ui/icons/Add';
 
 import DatasetTable from 'automan/dashboard/components/dataset/table';
 import { mainStyle } from 'automan/assets/main-style';
-
-import JobForm from 'automan/dashboard/components/dataset/form';
 
 class DatasetPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       target: -1,
-      datasetId: null,
-      formOpen: false
+      datasetId: null
     };
   }
-  show = () => {
-    this.setState({ formOpen: true });
-  };
-  hide = () => {
-    this.setState({ formOpen: false });
-  };
   render() {
     const { classes } = this.props;
     return (
       <Grid container spacing={24}>
-        <JobForm formOpen={this.state.formOpen} hide={this.hide} />
         <Grid item xs={12}>
           <Paper className={classes.root}>
             <DatasetTable target={-1} />
           </Paper>
-          <Fab
-            color="primary"
-            aria-label="Add"
-            className={classes.fab}
-            onClick={() => {
-              this.show();
-            }}
-          >
-            <Add />
-          </Fab>
         </Grid>
       </Grid>
     );
--- front/app/dashboard/components/job/entry.jsx
+++ front/app/dashboard/components/job/entry.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
 import Paper from '@material-ui/core/Paper';
-import Add from '@material-ui/icons/Add';
 import Grid from '@material-ui/core/Grid';
 
 import JobTable from 'automan/dashboard/components/job/table';
-import JobForm from 'automan/dashboard/components/job/form';
-
 import { mainStyle } from 'automan/assets/main-style';
 
 class JobPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formOpen: false,
       target: -1,
       jobId: null
     };
   }
-  show = () => {
-    this.setState({ formOpen: true });
-  };
-  hide = () => {
-    this.setState({ formOpen: false });
-  };
   render() {
     const { classes } = this.props;
     return (
       <Grid container spacing={24}>
         <Grid item xs={12}>
-          <JobForm formOpen={this.state.formOpen} hide={this.hide} />
           <Paper className={classes.root}>
             <JobTable onClickJob={this.handleClickJob} target={-1} />
           </Paper>
-          <Fab
-            color="primary"
-            aria-label="Add"
-            className={classes.fab}
-            onClick={this.show}
-          >
-            <Add />
-          </Fab>
         </Grid>
       </Grid>
     );
