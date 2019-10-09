eeff58d581d601b6bb9fe0175c2a8d1233f6f672
tsshimo <tshimo@asg.cs.tuat.ac.jp>
2019-10-09 08:13:30 +0000

    add: 'create annotation' in Dataset

--- front/app/dashboard/components/dataset/entry.jsx
+++ front/app/dashboard/components/dataset/entry.jsx
@@ -3,6 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
+import AnnotationForm from 'automan/dashboard/components/dataset/annotationForm'
 import DatasetTable from 'automan/dashboard/components/dataset/table';
 import { mainStyle } from 'automan/assets/main-style';
 
@@ -10,17 +11,31 @@ class DatasetPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      target: -1,
-      datasetId: null
+      dataset_id: null,
+      formOpen: false
     };
   }
+  show = (dataset_id) => {
+    this.setState({
+      dataset_id: dataset_id,
+      formOpen: true
+    });
+  }
+  hide = () => {
+    this.setState({ formOpen: false });
+  };
   render() {
     const { classes } = this.props;
     return (
       <Grid container spacing={24}>
+        <AnnotationForm
+          hide={this.hide}
+          formOpen={this.state.formOpen}
+          dataset_id={this.state.dataset_id}
+        />
         <Grid item xs={12}>
           <Paper className={classes.root}>
-            <DatasetTable target={-1} />
+            <DatasetTable show={this.show}/>
           </Paper>
         </Grid>
       </Grid>
