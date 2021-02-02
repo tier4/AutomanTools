d793933d7e7d33ffb7fd2999dc1ba4b4153d2a37
Yuto Jumonji <rekkuuzadx@yahoo.co.jp>
2021-02-02 17:01:33 +0900

    fix: material-ui spacing.unit

--- front/app/dashboard/components/project/storage_form.jsx
+++ front/app/dashboard/components/project/storage_form.jsx
@@ -27,14 +27,14 @@ const styles = theme => ({
     width: '90%'
   },
   button: {
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing(1)
   },
   backButton: {
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing(1)
   },
   instructions: {
-    marginTop: theme.spacing.unit,
-    marginBottom: theme.spacing.unit
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1)
   }
 });
 
